11ddaddc2da625f690e49e0b8e04449369e52a20
maryna.yelakova <maryna.yelakova@gmail.com>
2017-03-26 00:42:53 +0200

    add icon to the weather dashboard

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-app.set('port', (process.env.PORT || 1000));
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(__dirname + '/../frontend/build'));
 app.listen(app.get('port'), function () {
-    console.log('Example app listening on port 1000!');
+    console.log('Example app listening on port 3000!');
 });
 
 
